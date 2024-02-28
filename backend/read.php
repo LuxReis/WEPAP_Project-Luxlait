30bc7a0e7085ec026cb138038566f6d027224a45
LuxReis <luxusreis@gmail.com>
2024-02-28 09:20:36 +0100

    Create read.php
    - check if the GET functionTyp and payload isset
    - switch for the function
    - Create login function
    -Create checkRegister function (check if the user already exist before register it)

---
+++ backend/read.php
@@ -0,0 +1,32 @@
+<?php
+
+    if(!isset($_GET['functionTyp']) || !isset($_GET['payload'])){
+        // Error: There is no function type or payload provided.
+        echo "Error: No function type and or payload provided.";
+        exit();     
+    }
+
+    //Switch for payload
+    switch ($_GET['payload']) {
+        case 'login':
+            login($payload);
+            break;
+        case 'checkRegister':
+            checkRegister($payload);
+            break;
+
+        default:
+            // Error: There is no payload provided.
+            echo "Error: No payload provided.";
+            exit();
+    }
+
+    function login($payload){
+
+    }
+
+    function checkRegister($payload){
+
+    }
+
+?>
