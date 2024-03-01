d565788ec5fd9355c263579b6609b393d3252314
Luxbaut <frith033@school.lu>
2024-03-01 09:26:38 +0100

    Luxlait SQL
    Updated the tblUser Table

--- backend/db/sql_luxlait.sql
+++ backend/db/sql_luxlait.sql
@@ -1,5 +1,9 @@
 CREATE TABLE tblUser(
     idUsername VARCHAR(10) UNIQUE,
     dtPassword VARCHAR(40) NOT NULL,
-
+    dtEmail VARCHAR(50) NOT NULL,
+    dtTelephone VARCHAR(9),
+    dtPLZ VAR(6) NOT NULL,
+    dtHouseNR VARCHAR(3) NOT NULL,
+    dtStreet VARCHAR(15) NOT NULL,
 );
