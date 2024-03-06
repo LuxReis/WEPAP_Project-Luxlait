e6f93e758e70b1e3f61a9c084d5e9dd09989a5a5
LuxJoghurt <ribpe261@school.lu>
2024-03-06 08:47:22 +0100

    Update create.php

--- backend/functions/db_createFunctions.php
+++ backend/functions/db_createFunctions.php
@@ -1,7 +1,12 @@
 <?php
-function insertUser() {
+function insertUser($payload) {
     $query = "INSERT INTO tblUser (idUsername, dtFirstName, dtLastName, dtPassword, dtEmail, dtTelephone, dtPLZ, dtHouseNR, dtStreet, fiCountry)";
-    
+
+}
+
+function insertProduct($payload) {
+    $query = "INSERT INTO tblProduct (dtProduct, dtPrice, dtImage)";
+
 }
 
 ?>
