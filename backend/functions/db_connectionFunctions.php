90c8ff049f22722944a0f89ddfd453a6261ea212
Luxlait <timfrisch530@gmail.com>
2024-03-06 08:15:51 +0100

    Update db_connectionFunctions.php
    Add the function for connection

---
+++ backend/functions/db_connectionFunctions.php
@@ -0,0 +1,13 @@
+<?php
+// Funktion zur Herstellung der Datenbankverbindung
+function dbConnections()
+{
+    require_once 'db.credentials.php';
+    $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PW, DB_NAME);
+    if (mysqli_connect_errno()) {
+        die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+    } else {
+        mysqli_set_charset($dbc, 'utf8');
+        return $dbc;
+    }
+}
