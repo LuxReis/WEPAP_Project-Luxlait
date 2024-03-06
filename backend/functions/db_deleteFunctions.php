b601c983c3659fd9f5dd5a4e6c7ff351f6504953
Luxlait <timfrisch530@gmail.com>
2024-03-06 08:35:03 +0100

    Write Exemple for Delete Functions
    Wrote the deleteProduct and deleteUser those are just exemples

---
+++ backend/functions/db_deleteFunctions.php
@@ -0,0 +1,11 @@
+<?php
+function deleteUser() {
+    $query = "DELETE FROM tblUser WHERE idUsername = ";
+    
+}
+
+function deleteProduct() {
+    $query = "DELETE FROM tblProduct WHERE idProduct = ";
+    
+}
+?>
