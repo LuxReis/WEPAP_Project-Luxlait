b0d11e8ab9d306d8962062b9ece509042da3d0a5
GamMi625 <mihagami253@gmail.com>
2024-03-06 08:26:11 +0100

    Creating db_readFunctions.php, db_updateFunctions.php
    Creating db_readFunctions.php and db_updateFunctions.php

--- backend/db/db_credentials.php
+++ backend/db/db_credentials.php
@@ -1,6 +1,6 @@
 <?php
     define('DB_HOST', 'localhost'); // set database host (normally localhost)
     define('DB_USER', 'luxlait'); 	// set database user
-    define('DB_PASSWORD', 'LuxlaitIsTheBestWEPAP2024'); // set database password
+    define('DB_PW', 'LuxlaitIsTheBestWEPAP2024'); // set database password
     define('DB_NAME', 'luxlait'); // set database name 
 ?>
