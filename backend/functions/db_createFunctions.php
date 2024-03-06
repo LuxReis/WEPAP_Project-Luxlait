0bd7cd1ec7eafc80541f669802bc2f2c449eede4
Luxlait <timfrisch530@gmail.com>
2024-03-06 08:48:54 +0100

    Include the connection
    Added all the include to the Connenction

--- backend/functions/db_createFunctions.php
+++ backend/functions/db_createFunctions.php
@@ -1,4 +1,5 @@
 <?php
+include_once backend/functions/db_connectionFunctions.php;
 function insertUser() {
     $query = "INSERT INTO tblUser (idUsername, dtFirstName, dtLastName, dtPassword, dtEmail, dtTelephone, dtPLZ, dtHouseNR, dtStreet, fiCountry)";
     
--- backend/functions/db_updateFunctions.php
+++ backend/functions/db_updateFunctions.php
@@ -1,4 +1,5 @@
 <?php
+include_once backend/functions/db_connectionFunctions.php;
     function updateUser() {
         
         
