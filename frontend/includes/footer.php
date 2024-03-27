b1763a6927fd23012ff1ab9a616d322d1670f555
Luxlait <timfrisch530@gmail.com>
2024-03-27 08:59:40 +0100

    Index + footer + nav zum testen
    Added the pages so i can see it Xampp

---
+++ frontend/includes/footer.php
@@ -0,0 +1,21 @@
+  <footer>
+      <div class="container mt-5 mb-5">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <hr>
+            <h6>About</h6>
+            <p class="text-justify">At Luxlait, we share a common ambition that motivates each of our employees at all levels of
+                 production: to be the benchmark in the dairy products sector in Europe.</p>
+          </div>
+          
+          <div class="col-xs-6 col-md-3">
+          <h6>Our products</h6>
+          
+          </div>
+          <div class="col-xs-6 col-md-3">
+          
+          </div>
+        </div>
+        <hr>
+      </div>
+</footer>
