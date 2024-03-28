ad9cb891e393d64529e27bc25e16705ff4e93e4e
LuxJoghurt <ribpe261@school.lu>
2024-03-28 11:05:47 +0100

    Update contact.php

--- backend/include/contact.php
+++ backend/include/contact.php
@@ -1,43 +1,28 @@
 <?php
-      $staffMembers = [
-          [
-            'name' => 'Guo Kaidi',
-            'email' => 'guoka342@school.lu',
-            'profile' => 'profil.png',
-            'role' => 'Head Chef'
-          ],
-          [
-            'name' => 'Ribeiro Costa Pedro',
-            'email' => 'ribpe261@school.lu',
-            'profile' => 'profil.png',
-            'role' => 'Project Manager'
-          ],
-          [
-            'name' => 'Frisch Tim',
-            'email' => 'friti725@school.lu',
-            'profile' => 'profil.png',
-            'role' => 'Web Designer'
-          ],
-          [
-            'name' => 'Friederici Thibaut',
-            'email' => 'frith033@school.lu',
-            'profile' => 'profil.png',
-            'role' => 'Database Administrator'
-          ],
-          [
-            'name' => 'Gamass Mihails',
-            'email' => 'gammi625@school.lu',
-            'profile' => 'profil.png',
-            'role' => 'Scrum Master'
-          ],
-          [
-            'name' => 'Formica Luca',
-            'email' => 'forlu102@school.lu',
-            'profile' => 'profil.png',
-            'role' => 'IT Administrator'
-          ]
-      ];
-      
-      $staffJson = json_encode($staffMembers);
-      echo $staffJson;
+$conn = mysqli_connect('89.58.47.144', 'luxLaitUser', 'luxLaitPass', 'dbLuxLait');
+
+// Verbindung überprüfen
+if (!$conn) {
+    die("Verbindung zur Datenbank fehlgeschlagen: " . mysqli_connect_error());
+}
+
+$sql = "SELECT dtFirstName AS first_name, dtLastName AS last_name, dtEmail AS email, dtRole AS role FROM tblUser WHERE dtRole NOT IN ('Admin', 'Staff', 'User')";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    $staffMembers = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+        $staffMembers[] = [
+            'name' => $row['first_name'] . ' ' . $row['last_name'],
+            'email' => $row['email'],
+            'role' => $row['role']
+        ];
+    }
+    $staffJson = json_encode($staffMembers);
+    echo $staffJson;
+} else {
+    echo "Keine Mitarbeiter gefunden";
+}
+
+mysqli_close($conn);
 ?>
