3688cbb51c3bc75db16dd435993ee450fc098a96
LuxReis <luxusreis@gmail.com>
2024-03-29 08:42:00 +0100

    Update db_credentials.php
    - change the credentials with noah`s db

--- backend/db/db_credentials.php
+++ backend/db/db_credentials.php
@@ -1,8 +1,8 @@
 <?php
 
-    define('DB_HOST', '89.58.47.144'); // set database host (normally localhost)
-    define('DB_USER', 'luxLaitUser'); 	// set database user
-    define('DB_PASSWORD', 'luxLaitPass'); // set database password
-    define('DB_NAME', 'dbLuxlait'); // set database name 
+    define('DB_HOST', '193.41.237.172'); // set database host (normally localhost)
+    define('DB_USER', 'luxlait_project'); 	// set database user
+    define('DB_PASSWORD', 'luxlait_project_123'); // set database password
+    define('DB_NAME', 'luxlait_project'); // set database name 
 
 ?>
