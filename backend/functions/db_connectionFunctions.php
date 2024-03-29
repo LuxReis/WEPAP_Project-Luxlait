b7d578b5196a7877b1669c3d0418fa88d1e79006
Luxbaut <frith033@school.lu>
2024-03-29 09:02:37 +0100

    Small error with the password definition

--- backend/functions/db_connectionFunctions.php
+++ backend/functions/db_connectionFunctions.php
@@ -3,7 +3,7 @@
 function dbConnections()
 {
     require_once 'db.credentials.php';
-    $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PW, DB_NAME);
+    $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
     if (mysqli_connect_errno()) {
         die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
     } else {
