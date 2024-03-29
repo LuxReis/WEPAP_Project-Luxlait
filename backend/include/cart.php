01964df2942cbe895ec927b5ea5bef0ba5fb6ce9
Luxbaut <frith033@school.lu>
2024-03-29 13:04:10 +0100

    Added cart.php & cart.html

--- backend/include/cart.php
+++ backend/include/cart.php
@@ -1,19 +1,39 @@
 <?php
 
-require_once "backend/functions/db_connectionFunctions";
+require_once "backend/functions/db_connectionFunctions.php";
 $dbc = dbConnections();
 
 if (isset($_GET['username-cart'])) {
     $username = $_GET['username-cart'];
+    $cart = [];
+    $product = [];
 
-    $querySelect = "SELECT tbldtAmount, fiProduct, fiUsername FROM tblShoppingCart WHERE fiUsername = ?";
+    $querySelect = "SELECT dtAmount, dtProduct
+                    FROM tblShoppingCart
+                    INNER JOIN tblProduct ON idProduct = fiProduct
+                    INNER JOIN tblType ON idType = fiType
+                    INNER JOIN tblCategory ON idCategory = fiCategory
+                    WHERE fiUsername = ?";
+
     $stmt = mysqli_prepare($dbc, $querySelect);
     mysqli_stmt_bind_param($stmt, "s", $username);
-
     mysqli_stmt_execute($stmt);
+    mysqli_stmt_bind_result(
+        $stmt,
+        $dtAmount,
+        $dtProduct
+    );
+
+    while (mysqli_stmt_fetch($stmt)) {
+        $cart[] = array(
+            'amount' => $dtAmount,
+            'product' => $dtProduct
+        );
+    }
+
+    echo json_encode($cart);
     mysqli_stmt_close($stmt);
 }
-
 
 // Bild als Inspiration
 //https://miro.medium.com/v2/resize:fit:1024/1*2U7hReW6WUfLkoNfZbfekQ.png
