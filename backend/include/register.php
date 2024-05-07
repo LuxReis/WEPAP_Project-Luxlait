5ccb3568c1052b48c07c592a244b54b4afaaeea3
GamMi625 <mihagami253@gmail.com>
2024-05-08 00:49:06 +0200

    fix register

--- backend/include/register.php
+++ backend/include/register.php
@@ -1,8 +1,5 @@
 <?php
 // register.php from author @LuxKeis (Gamass Mihails)
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
     session_start();
     $url = getenv('JAWSDB_URL');
